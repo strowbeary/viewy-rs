0c64ad4a7ad6aea02185a89fd55faf2bbf8abcad
Rémi Caillot <remicaillot5@gmail.com>
2023-03-21 12:40:06 +0100

    build script fix

--- lib/build.rs
+++ lib/build.rs
@@ -11,7 +11,9 @@ use quote::format_ident;
 use quote::quote;
 
 fn path_from_icon(icons_folder_path: &PathBuf, icon_name: &str) -> String {
-    let file_content = match std::fs::read_to_string(icons_folder_path.join(format!("{}.svg", icon_name.to_kebab_case()))) {
+    let file_content = match std::fs::read_to_string(
+        icons_folder_path.join(format!("{}.svg", icon_name.to_kebab_case())),
+    ) {
         Ok(content) => content,
         // The icon doesn't exist for this size, so fall back to the other size.
         // Hopefully this doesn't loop forever.
@@ -19,30 +21,34 @@ fn path_from_icon(icons_folder_path: &PathBuf, icon_name: &str) -> String {
     };
     let inner_html = scraper::Html::parse_fragment(&file_content)
         .select(&scraper::Selector::parse("svg").expect("Failed getting svg"))
-        .map(|svg| {
-            svg.inner_html()
-        })
+        .map(|svg| svg.inner_html())
         .collect::<Vec<String>>()
         .join(" ");
     inner_html
 }
 
-pub fn generate_icon_pack(icon_pack_name: &str, only_stroked: bool, icons_folder_path: &str, icon_name_prefix: Option<&str>) -> String {
+pub fn generate_icon_pack(
+    icon_pack_name: &str,
+    only_stroked: bool,
+    icons_folder_path: &str,
+    icon_name_prefix: Option<&str>,
+) -> String {
     let icon_pack_name_ident = format_ident!("{}", icon_pack_name);
-    let path = Path::new(&env::var("CARGO_MANIFEST_DIR").expect("Can't read CARGO_MANIFEST_DIR env var"))
-        .join(icons_folder_path);
-    let lucide_icons =
-        std::fs::read_dir(&path).expect("Failed reading icons directory");
+    let path =
+        Path::new(&env::var("CARGO_MANIFEST_DIR").expect("Can't read CARGO_MANIFEST_DIR env var"))
+            .join(icons_folder_path);
+    let lucide_icons = std::fs::read_dir(&path).expect("Failed reading icons directory");
 
     let icon_names = lucide_icons
-        .map(|lucide_icon| {
-            let icon_file_name = lucide_icon
+        .map(|icon_file| {
+            icon_file
                 .expect("Failed reading icons directory entry")
                 .file_name()
                 .into_string()
-                .expect("Icon svg filename is not valid UTF-8");
-            icon_file_name.split('.').collect::<Vec<&str>>()[0].to_string()
+                .expect("Icon svg filename is not valid UTF-8")
         })
+        .filter(|icon_file_name| icon_file_name.ends_with("svg"))
+        .map(|icon_file_name| icon_file_name.split('.').collect::<Vec<&str>>()[0].to_string())
         .collect::<Vec<String>>();
 
     let icon_kind_enum_inner = icon_names.iter().map(|kind| {
@@ -121,15 +127,20 @@ fn main() {
     let mut code = quote! {
          use crate::modifiers::DefaultModifiers;
     }
-        .to_string();
+    .to_string();
     code += &generate_icon_pack("Lucide", true, "assets/lucide/icons", None);
-    code += &generate_icon_pack("SimpleIcons", false, "assets/simple-icons/icons", Some("Icon"));
- //   code += &generate_icon_pack("ViewyIconPack", false, "assets/viewy-icon-pack", None);
+    code += &generate_icon_pack(
+        "SimpleIcons",
+        false,
+        "assets/simple-icons/icons",
+        Some("Icon"),
+    );
+    //   code += &generate_icon_pack("ViewyIconPack", false, "assets/viewy-icon-pack", None);
 
     std::fs::write(
         Path::new(&env::var("OUT_DIR").expect("Failed reading OUT_DIR environment variable"))
             .join("icons.rs"),
         code,
     )
-        .expect("Failed writing generated.rs");
+    .expect("Failed writing generated.rs");
 }
